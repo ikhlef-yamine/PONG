1d9aa1571f11c4b28de64cc80e87855e67a40a1b
yamine_ikhlef <yamineikhlef@yahoo.com>
2026-03-19 15:22:58 +0100

    Initialisation des constantes du projet

---
+++ constants.py
@@ -0,0 +1,16 @@
+"""
+ce module contiens toute les constante utiliser dans le projet
+"""
+
+
+__author__ = "ikhlef_y"
+
+
+# definire les constante pour la fenetre
+HHEIGHT = 600
+WIDTH = 800
+
+# definire les constante pour les couleur
+WHITE = (255, 255, 255)
+# definire les constante pour les raquette
+PADDLE_SPEED = 10
